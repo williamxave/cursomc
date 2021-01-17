27a065176572aa686714f20be01b0ef5f8e37d0a
William Bohn <xavegithub@gmail.com>
2021-01-17 17:14:51 -0300

    Projeto criado

---
+++ src/test/java/com/williambohn/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williambohn.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
