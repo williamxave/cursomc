0b8fa93833adb518bb1c1a47645144a9f55b48a8
William Bohn <xavegithub@gmail.com>
2021-01-21 18:46:39 -0300

    Endpoint/clientes/{id} disponivel

---
+++ src/main/java/com/williambohn/cursomc/resources/ClienteResource.java
@@ -0,0 +1,25 @@
+package com.williambohn.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.williambohn.cursomc.domain.Cliente;
+import com.williambohn.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
