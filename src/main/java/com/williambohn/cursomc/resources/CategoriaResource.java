116409f2166caa2f64c476f19416655df7c0ae54
William Bohn <xavegithub@gmail.com>
2021-01-23 15:04:41 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/williambohn/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/williambohn/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package com.williambohn.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.williambohn.cursomc.domain.Categoria;
 import com.williambohn.cursomc.services.CategoriaService;
@@ -18,10 +24,11 @@ public class CategoriaResource {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) { // Para que o Spring saiba que o ID da url vai vir do ID do metodo precisa usar a anotacao @PathVariable
+	public ResponseEntity<?> find(@PathVariable Integer id) { // Para que o Spring saiba que o ID da url vai vir do ID
+																// do metodo precisa usar a anotacao @PathVariable
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
+
 		/*
 		 * ResponseEntity ele encapsula uma resposta HTTP pra um serviço REST <?> é para
 		 * retorna qualquer tipo, pode ou nao encontrar a resposta
@@ -31,8 +38,27 @@ public class CategoriaResource {
 		 * retorna no corpo da consulta o objeto pesquisado
 		 * 
 		 */
-		
-		
+
+	}
+	
+	/*
+	 * Metodo para inserir uma categoria no banco de dados
+	 * 
+	 * @RequestBody transforma o objeto java em json automaticamente
+	 * 
+	 * Por padrao a requisicao Http deve ser montada assim, como resposta no corpo deve aparecer a URI do novo obj regitraddo alem do codio http 201(CREATED)
+	 * para pegar essa nova URI usa-se URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	 * 
+	 * e para mostrar o codigo de criacao usa-se o ResponseEntity.created(aqui dentro vem a sua uri ex: uri) e o . build(); builda para voc
+	 * 
+	 */
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
--- src/main/java/com/williambohn/cursomc/services/CategoriaService.java
+++ src/main/java/com/williambohn/cursomc/services/CategoriaService.java
@@ -13,15 +13,24 @@ import com.williambohn.cursomc.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 
 	@Autowired // instancia automaticamente
+	
 	private CategoriaRepository repo;
 
 	/*
 	 * Busca no banco de dados um objeto pelo ID, se nao tiver instacia do msmo
 	 * retorna null
 	 */
+	
 	public Categoria buscar(Integer id) {   
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	//Insere uma categoria no banco de dados
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null); // Verifica se o id nao esta null, se estiver null, é feita o insert, se o id nao for null, o sistema considera uma atualizacao
+		return repo.save(obj);
 	}
 }
