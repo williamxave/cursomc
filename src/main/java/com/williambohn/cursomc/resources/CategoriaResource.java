f8c1a711a8093fe1ef894e747749e879282e2c2d
William Bohn <xavegithub@gmail.com>
2021-01-24 14:53:47 -0300

    Paginacao com paramentros opcionais na requisicao

--- src/main/java/com/williambohn/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/williambohn/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -82,11 +84,32 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	//Retorna todas Categorias
+	/*Retorna todas Categorias
+	 *  
+	 *  stream()  Recurso do java 8 para percorer uma lista
+	 *  map() Faz uma operação para cada elemento da lista
+	 *  -> arrow function; ou seja uma operação anônima que recebe um OBJ e o transforma em new CategoriaDTO(obj) e recebe o obj como argumento
+	 *  
+	 *  para passar o list.stream().map(obj -> new CategoriaDTO(obj)) para o tipo lista para isso usa-se  collect(Collectors.toList())
+	 *  com isso em uma linha dapra converter uma lista para outra em uma linha
+	 */
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage( 
+		@RequestParam(value = "page", defaultValue = "0") 			Integer page,
+		@RequestParam(value = "linesPerPage", defaultValue = "24") 	Integer linesPerPage,
+		@RequestParam(value = "orderBy", defaultValue = "nome")		String orderBy ,
+		@RequestParam(value = "direction", defaultValue = "ASC")	String direction){
+		Page<Categoria> list = service.findPage(page, linesPerPage, direction, orderBy);
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
+				return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/com/williambohn/cursomc/services/CategoriaService.java
+++ src/main/java/com/williambohn/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.williambohn.cursomc.domain.Categoria;
@@ -60,6 +63,23 @@ public class CategoriaService {
 	// Retorna todas Categorias
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	
+	/*Page é uma classe do Spring, para paginar os resultados
+	 * ex: se voc tiver 1M de categorias e precisa retornar todas, vai usar muito processamento, entao voc pagina isso 
+	 * como por ex: 20 categorias por pagina que fica safe
+	 */
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String direction, String orderBy){
+		
+		/*Para retornar uma pagina de dados usamos PageRequesttbm do spring data, ele vai preparar os informações para a pesquisa 
+		 * para isso usar PageRequest.of; a direçao vem em forma de String para converter usa-se Direction.valueOf(direction) que tbm é do Spring data e converte para o tipo direction
+		 * 
+		 * e retorna repo.findAll(pageRequest) automaticamente o metodo findAll vai considerar  pageRequest como argumento(uma sobrecarga de metodo) e vai retornar a pagina
+		 * 
+		 */
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction), orderBy );
+		return repo.findAll(pageRequest);
 	}
 	
 }
