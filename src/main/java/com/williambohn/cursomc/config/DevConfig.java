22fe4371ec691c4e7857d6101a50ca3775bdbfc2
William Bohn <xavegithub@gmail.com>
2021-02-02 20:27:16 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/williambohn/cursomc/config/DevConfig.java
+++ src/main/java/com/williambohn/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.williambohn.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,16 +11,24 @@ import org.springframework.context.annotation.Profile;
 import com.williambohn.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
-	
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
-		dbService.instanciateTesteDatabs();
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		} else {
+			dbService.instanciateTesteDatabs();
+		}
 		return true;
 	}
-	
+
 }
